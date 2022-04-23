fd86a6b277fff890e95438b32119b2f8251b71e6
Four <f0uronvacation@protonmail.com>
2022-04-23 14:29:41 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // ===========================================
-// Clasiccal function for getting a pseudo-random number between with a min and max
+// Clasiccal function for getting a pseudo-random number between a min and max value
 function randomNumber(min, max) { 
     return Math.random() * (max - min) + min;
 }
